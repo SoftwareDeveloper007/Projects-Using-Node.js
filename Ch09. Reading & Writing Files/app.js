9ecdc8b6b177c085a5ca305cb067543cd389719f
Taaniel Magi <28940651+YellowBackCorn@users.noreply.github.com>
2017-10-31 16:52:15 +0300

    6th Commit

--- Ch09. Reading & Writing Files/app.js
+++ Ch09. Reading & Writing Files/app.js
@@ -1,6 +1,8 @@
 var fs = require('fs');
 
-var readMe = fs.readFileSync('readMe.txt', 'utf8');
-fs.writeFileSync('writeMe.txt', readMe);
-console.log(readMe);
+var readMe = fs.readFileSync('readMe.txt', 'utf8', function (err, data) {
+    console.log(data);
+    fs.writeFile('writeMe.txt', data);
+});
+
 // code
