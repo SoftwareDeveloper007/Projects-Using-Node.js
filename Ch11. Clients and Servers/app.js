ae8d1e0986c05f91e2ccf70cff2a52b5e8728007
Taaniel Magi <28940651+YellowBackCorn@users.noreply.github.com>
2017-10-31 17:16:58 +0300

    9th Commit

--- Ch11. Clients and Servers/app.js
+++ Ch11. Clients and Servers/app.js
@@ -1,4 +1,10 @@
-var fs = require('fs');
+var http = require('http');
 
-fs.rmdirSync('stuff');
-fs.mkdirSync('stuff');
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hey ninjas');
+});
+
+server.listen(3000, '127.0.0.1');
+
+console.log('yo dawgs, now listening to port 3000'); 
