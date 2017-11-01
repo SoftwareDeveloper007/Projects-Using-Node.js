fb40d3716db973a251d729ec81e6ceaa28dc5def
Taaniel Magi <28940651+YellowBackCorn@users.noreply.github.com>
2017-11-01 15:02:06 +0300

    2nd Commit

---
+++ 04. Serving HTML Files/server.js
@@ -0,0 +1,48 @@
+// Array of Mime Types
+var mimeTypes = {
+    "html": 'text/html',
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpeg",
+    "png": "image/png",
+    "js": "text/javascript",
+    "css": "text/css"
+};
+
+// Create Server
+http.createServer(function () {
+    var uri = url.parse(req.url).pathname;
+    var fileName = path.join(process.cwd(), unescape(uri));
+    console.log('Loading ' + uri);
+    var stats;
+
+    try{
+        stats = fs.lstatSync(fileName);
+    }
+    catch{
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.write('404 Not Found\n');
+        res.end();
+        return;
+    }
+
+    // Check if file/directory
+    if(stats.isFile()){
+        var mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]];
+        res.writeHead(200, {'Content-Type': mimeType});
+
+        var fileStream = fs.createReadStream(fileName);
+        fileStream.pipe(res);
+    }
+    else if(stats.isDirectory()){
+        res.writeHead(302, {
+            'Location': 'index.html'
+        });
+        res.end();
+    }
+    else {
+        res.writeHead(500, {'Content-Type': 'text/plain'});
+        res.write('500 Internal Error\n');
+        res.end();
+    }
+
+}).listen(3000);
